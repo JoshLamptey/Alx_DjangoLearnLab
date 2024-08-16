986fb0ca6bbac07e65316ec3f2490e045cdddda8
JoshLamptey <joshlamptey75@gmail.com>
2024-08-15 23:25:28 -1200

    run it up

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -17,3 +17,37 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('list_books')
+        else:
+            return render(request,'login.html', {'error': 'Invalid credentials.'})
+
+
+    else:
+        return render(request, 'relationship_app/login.html')
+    
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        
+    else:
+        form = UserCreationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'relationship_app/register.html', context)
+
+#break-off point. I'll continue tomorrow
