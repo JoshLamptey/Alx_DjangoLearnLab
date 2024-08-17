ced8c8c32be86e6e78d8edcdaa664f09df6183df
JoshLamptey <joshlamptey75@gmail.com>
2024-08-17 00:06:11 -1200

    last but not the least

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from dango.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -10,6 +11,13 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    class Meta:
+        permissions = [
+            ('can_add_book', "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book")
+        ]
 
     def __str__(self):
         return self.title
@@ -28,3 +36,17 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import login,authenticate
 from django.contrib.auth.decorators import user_passes_test
+from .forms import BookForm
+
 
 #create your views here
 def list_books(request):
@@ -51,3 +54,26 @@ def register(request):
     return render(request, 'relationship_app/register.html', context)
 
 #break-off point. I'll continue tomorrow
+#helper functions to check the roles of the users
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+     return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+     return user.userprofile.role == 'Member'
+     
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
