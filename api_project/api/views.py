c6ffbb50f2d7b8e7f147dd6c7a99b777f0f022d6
JoshLamptey <joshlamptey75@gmail.com>
2024-08-29 08:05:52 -1200

    back to the grind

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,6 +6,6 @@ from rest_framework import generics
 # Create your views here.
 
 
-class BookList(generics.ListApiView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
