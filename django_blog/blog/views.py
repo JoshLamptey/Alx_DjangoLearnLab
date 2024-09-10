0737de76b14ff19da924ee8cbdeac8ad2585aade
JoshLamptey <joshlamptey75@gmail.com>
2024-09-09 23:15:06 -1200

    a hail mary

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    if request.POST =='POST':
+        form = UserCreationForm()
+        if form.is_valid():
+            form.save()
+        messages.success(request, 'Account created successfully ')
+    else:
+     form = UserCreationForm()
+     context = {
+        'form' : form
+     }   
+     return render(request,'register.html', context)
