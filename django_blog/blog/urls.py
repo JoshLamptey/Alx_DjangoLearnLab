18301461018a41e5c2c593031021a91822030abb
JoshLamptey <joshlamptey75@gmail.com>
2024-09-12 21:07:13 -1200

    bro

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,10 +6,10 @@ from . import views
   
 
 urlpatterns = [  
-    path("/login", views.login,  name='login'),
+    path("login/", views.login,  name='login'),
     path("login_user/", auth_views.LoginView.as_view(template_name='templates/login.html'), name='login'),
-    path("/logout", auth_views.LogoutView.as_view(template_name='templates/logout.html'), name='logout'),
-    path("/profile",views.profile, name = 'profile' ),
+    path("logout/", auth_views.LogoutView.as_view(template_name='templates/logout.html'), name='logout'),
+    path("profile/",views.profile, name = 'profile' ),
     path("admin/", admin.site.urls),  
-    path("register", register, name = 'register')
+    path("register/", register, name = 'register')
 ]
