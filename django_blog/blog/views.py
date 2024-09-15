0f767a23732d73609f177f316b76cb426d65b67a
JoshLamptey <joshlamptey75@gmail.com>
2024-09-14 20:50:18 -1200

    bro

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post 
+
 
 # Create your views here.
 
 def register(request):
-    if request.POST =='POST':
+    if request.method.POST =='POST':
         form = UserCreationForm()
         if form.is_valid():
             form.save()
@@ -14,3 +18,89 @@ def register(request):
         'form' : form
      }   
      return render(request,'register.html', context)
+
+class PostListView(generics.ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+
+class PostDetailView(generics.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    fields = ['title', 'content']
+
+    
+
+class PostCreateView(generics.CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostUpdateView(generics.UpdateView, LoginRequiredMixin, UserPassesTestMixin,):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(generics.DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
