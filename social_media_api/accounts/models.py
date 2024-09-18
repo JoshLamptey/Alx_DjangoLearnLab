6c5bf8747f1c006fe2fdb1fbcda414775632ef33
JoshLamptey <joshlamptey75@gmail.com>
2024-09-18 04:01:39 -1200

    this week will claim my life

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     bio = models.TextField(max_length=500)
-    profile = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile = models.ImageField(upload_to="profile_picture/", blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
 
 
