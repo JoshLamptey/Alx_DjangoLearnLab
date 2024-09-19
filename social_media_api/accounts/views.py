a0ff43ccf8b249c2b1a351646d64c594a744c92d
JoshLamptey <joshlamptey75@gmail.com>
2024-09-19 05:31:15 -1200

    corrections and task 2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework import generics,permissions,serializer
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import CustomUserSerializer
+from .models import User
 
 # Create your views here.
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = CustomUserSerializer  
+    password = serializer.Charfield(max_length=10, blank=False)
+    permission_classes = [permissions.AllowAny]
+
+
+
+
+
+class UserLoginView(APIView):
+    def post(self,request):
+        user = get_user_model().objects.create_user
+        if user:
+            token, created = Token.objects.create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
+
+
+
+class UserListView(generics.GenericAPIView):
+    """
+    View to list all users in the system.
+    Only accessible to authenticated users.
+    """
+    queryset = User.objects.all()  # Query all users
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        """
+        Return a list of all users.
+        """
+        users = self.get_queryset()
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters, status, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Post, Comment
+from .serializer import PostSerializer, CommentSerializer
 
 # Create your views here.
+
+#creating viewsets to handle CRUD operations
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    
+    def get_queryset(self):
+        return Post.objects.all()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
